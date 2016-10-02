da24941484c89282c02bbe68bac2ee78e12888ba
Ophilia <ophiliadaniel@hotmail.com>
2016-10-02 07:40:05 +0300

    add new files

---
+++ Plot3.R
@@ -0,0 +1,32 @@
+## load the data
+rm(list = ls())
+data <- read.table("household_power_consumption.txt", header = T, 
+                   sep = ";", na.strings = "?")
+# convert the date variable to Date class
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+# Subset the data
+data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+# Convert dates and times
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+data$datetime <- as.POSIXct(data$datetime)
+
+# Plot 3
+
+globalActivePower <- as.numeric(data$Global_active_power)
+subMetering1 <- as.numeric(data$Sub_metering_1)
+subMetering2 <- as.numeric(data$Sub_metering_2)
+subMetering3 <- as.numeric(data$Sub_metering_3)
+
+par(oma=c(0,0,2,2))
+plot(data$Sub_metering_1 ~ data$datetime, type = "l", 
+     ylab = "Energy sub metering", xlab = "")
+lines(data$Sub_metering_2 ~ data$datetime, col = "Red")
+lines(data$Sub_metering_3 ~ data$datetime, col = "Blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  
+
+dev.copy(png, file = "plot3.png", height = 480, width = 480)
+dev.off()
